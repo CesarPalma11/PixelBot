435d2af9f51ed0e46a402c99064c9db714426938
CesarPalma11 <cesarpalmag16@gmail.com>
2025-12-03 01:13:25 -0300

    fix v2

---
+++ sett.py
@@ -0,0 +1,33 @@
+import os
+
+# Token de verificación del webhook (el que configuras en Meta)
+token = os.getenv("TOKEN")
+
+# Token permanente de WhatsApp API
+whatsapp_token = os.getenv("WHATSAPP_TOKEN")
+
+# Phone Number ID
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+
+# URL final que usa el bot para enviar mensajes
+whatsapp_url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
+
+# URL de un documento de prueba
+document_url = os.getenv("DOCUMENT_URL")
+
+# Stickers u otros medios
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
